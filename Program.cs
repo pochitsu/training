d4c94fa8c36de34598e4ae74333ef66dee190f7f
Phornchita Suttiborriban <phorhita.ed@gmail.com>
2024-06-06 19:42:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collection.Generic;
+using System.Ling;
+using System.Text;
+using System.Threading.Tasks;
+
+namspace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
